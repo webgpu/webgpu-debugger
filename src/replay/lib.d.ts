0e91e5721f96362a25deb04dcb579d5765389562
Gregg Tavares <gman@chromium.org>
2022-10-24 13:12:42 -0700

    Refactor typescript types for Replay code

--- src/replay/lib.d.ts
+++ src/replay/lib.d.ts
@@ -1,8 +1,26 @@
+/* 
+This file should probably be deleted.
+It exists because lib.js is not typescript
+so it's casually describing the parts of
+the replay code that the UI is accessing.
+*/
+
+export type CommandArgs = Record<string, any>;
+
 export type Command = {
   name: string;
-  args: any[];
+  args: CommandArgs;
 };
 
 export type Replay = {
   commands: Command[];
 };
+
+export type CommandBuffer = {
+  commands: Command[];
+};
+
+export type QueueSubmitArgs = {
+  commandBuffers: CommandBuffer[];
+};
+
