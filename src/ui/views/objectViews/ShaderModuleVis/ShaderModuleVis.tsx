74bb78dfcba7e444443f2a65a73518803927f93f
Gregg Tavares <gman@chromium.org>
2022-11-02 15:21:26 -0700

    Show shader source in ShaderModel

--- src/ui/views/objectViews/ShaderModuleVis/ShaderModuleVis.tsx
+++ src/ui/views/objectViews/ShaderModuleVis/ShaderModuleVis.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReplayShaderModule } from '../../../../replay';
 import { ValueObject } from '../../../components/Value/Value';
 
+// This is kind of a hack in that it's not taking the tab size into account.
+// At stop point, instead of a `<pre>` tag we'll use a `<textarea>` to allow you
+// to edit or else a fancier editor like CodeMirror or monaco.
+function optionallyRemoveLeadingWhitespace(removeLeadingWhitespace: boolean, s: string) {
+    if (removeLeadingWhitespace) {
+        const allMatches = [...s.matchAll(/^[ \t]+/gm)];
+        if (allMatches) {
+            const trimLength = allMatches.reduce(
+                (shortest, m) => Math.min(shortest, m[0].length),
+                Number.MAX_SAFE_INTEGER
+            );
+            const lines = s.split('\n');
+            lines.forEach((line, ndx) => {
+                if (line.length >= trimLength) {
+                    lines[ndx] = line.substring(trimLength);
+                }
+            });
+            s = lines.join('\n');
+        }
+    }
+    return s;
+}
+
 export default function ShaderModuleVis({ data }: { data: ReplayShaderModule }) {
+    const { desc } = data;
+    const [raw, setRaw] = useState(false);
+
     return (
         <div className="spector2-vis">
-            <ValueObject data={data} />
+            <ValueObject data={desc.hints || {}} />
+            <input type="checkbox" checked={raw} onChange={event => setRaw(event.target.checked)} />
+            <pre>{optionallyRemoveLeadingWhitespace(!raw, desc.code)}</pre>
         </div>
     );
 }
