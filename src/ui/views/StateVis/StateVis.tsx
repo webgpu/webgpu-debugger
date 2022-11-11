3f39f334cc58d94ebc5b8af41c7bc8f99c99e6ee
Brandon Jones <tojiro@gmail.com>
2022-11-11 11:38:05 -0800

    Add preview attrib setting to states with a render pipeline in them

--- src/ui/views/StateVis/StateVis.tsx
+++ src/ui/views/StateVis/StateVis.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ReplayRenderPipeline } from '../../../replay';
 import JsonValue from '../../components/JsonValue/JsonValue';
+import { RenderPipelinePreviewLayoutVis } from '../../components/RenderPipelinePreviewLayoutVis/RenderPipelinePreviewLayoutVis';
 
 /* it's not clear what this is ATM. */
 interface StateVisProps {
@@ -7,8 +9,19 @@ interface StateVisProps {
 }
 
 export default function StateVis({ data }: StateVisProps) {
+    const [previewOpen] = useState(false);
+
     return (
         <div className="spector2-vis">
+            {data?.pipeline instanceof ReplayRenderPipeline &&
+                <div>
+                    <details open={previewOpen}>
+                        <summary><b>Render pipeline preview attributes</b></summary>
+                        <RenderPipelinePreviewLayoutVis key={data.pipeline.replayObjectKey} data={data.pipeline.previewLayout} />
+                    </details>
+                    <hr/>
+                </div>
+            }
             <JsonValue data={data} />
         </div>
     );
